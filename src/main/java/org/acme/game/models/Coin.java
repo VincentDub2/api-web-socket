c07185987110a2fa22902201b39eb56bf085d860
vincent <cerise.litchis.0z@icloud.com>
2024-03-02 10:50:54 +0100

    - Ajout des setters et getters

--- src/main/java/org/acme/game/models/Coin.java
+++ src/main/java/org/acme/game/models/Coin.java
@@ -10,4 +10,27 @@ public class Coin {
         this.y = y;
         this.collected = false;
     }
+
+    public void setCollected(boolean collected) {
+        this.collected = collected;
+    }
+    public boolean isCollected() {
+        return collected;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
 }

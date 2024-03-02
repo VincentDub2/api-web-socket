f1595140311032ca1ad4c99bcd5e0dfd5eb830fa
vincent <cerise.litchis.0z@icloud.com>
2024-03-02 10:32:23 +0100

    - Ajout des propriétés de base de la carte

--- src/main/java/org/acme/game/models/Map.java
+++ src/main/java/org/acme/game/models/Map.java
@@ -1,4 +1,12 @@
 package org.acme.game.models;
-
+import java.util.List;
 public class Map {
+    private List<Wall> walls;
+    private List<Coin> coins;
+
+    public Map(List<Wall> walls, List<Coin> coins) {
+        this.walls = walls;
+        this.coins = coins;
+    }
+
 }

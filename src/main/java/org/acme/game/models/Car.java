cceacf37267d06a2d5e9e60e53414e12e93ff15d
vincent <cerise.litchis.0z@icloud.com>
2024-03-02 10:54:26 +0100

    - Ajout des setters et getters. Ajout de la propriété vitesse

--- src/main/java/org/acme/game/models/Car.java
+++ src/main/java/org/acme/game/models/Car.java
@@ -4,6 +4,8 @@ public class Car {
     private String id;
     private int x;
     private int y;
+
+    private int speed;
     private int direction; // 0: north, 1: east, 2: south, 3: west
     private int score;
 
@@ -11,10 +13,56 @@ public class Car {
         return id;
     }
 
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    public int getSpeed() {
+        return speed;
+    }
+
+    public void setSpeed(int speed) {
+        this.speed = speed;
+    }
+
+    public int getDirection() {
+        return direction;
+    }
+
+    public void setDirection(int direction) {
+        this.direction = direction;
+    }
+
+    public int getScore() {
+        return score;
+    }
+
+    public void setScore(int score) {
+        this.score = score;
+    }
+
+
     public Car(String id, int x, int y) {
         this.id = id;
         this.x = x;
         this.y = y;
+        this.speed = 1; // Initial speed
         this.direction = 0; // Initial direction facing north
         this.score = 0;
     }

817a92d774aea287718857ddd930e0f274f11978
vincent <cerise.litchis.0z@icloud.com>
2024-03-02 10:27:39 +0100

    - Creation du Endpoint(/game) a completer plus tard

--- src/main/java/org/acme/game/websocket/WebSocketServer.java
+++ src/main/java/org/acme/game/websocket/WebSocketServer.java
@@ -1,4 +1,49 @@
 package org.acme.game.websocket;
 
+
+import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.websocket.*;
+import jakarta.websocket.server.ServerEndpoint;
+
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+@ServerEndpoint("/game")
+@ApplicationScoped
 public class WebSocketServer {
+
+    Map<String, Session> sessions = new ConcurrentHashMap<>();
+
+    @OnOpen
+    public void onOpen(Session session) {
+        sessions.put(session.getId(), session);
+        // Envoyer l'état initial du jeu au client connecté
+    }
+
+    @OnClose
+    public void onClose(Session session) {
+        sessions.remove(session.getId());
+        // Gérer la déconnexion du client
+    }
+
+    @OnError
+    public void onError(Session session, Throwable throwable) {
+        // Gérer les erreurs
+    }
+
+    @OnMessage
+    public void onMessage(String message, Session session) {
+        // Traiter les messages reçus des clients
+    }
+
+    // Méthode pour envoyer des messages à tous les clients connectés
+    public void broadcast(String message) {
+        sessions.values().forEach(s -> {
+            s.getAsyncRemote().sendObject(message, result -> {
+                if (result.getException() != null) {
+                    // Gérer les erreurs d'envoi de message
+                }
+            });
+        });
+    }
 }

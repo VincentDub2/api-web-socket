876a8c16fd09b43421b2f7d0bea5ad862837c28a
vincent <cerise.litchis.0z@icloud.com>
2024-03-02 10:32:13 +0100

    - Ajout des propriétés de base de coin

--- src/main/java/org/acme/game/models/Coin.java
+++ src/main/java/org/acme/game/models/Coin.java
@@ -1,4 +1,13 @@
 package org.acme.game.models;
 
 public class Coin {
+    private int x;
+    private int y;
+    private boolean collected;
+
+    public Coin(int x, int y) {
+        this.x = x;
+        this.y = y;
+        this.collected = false;
+    }
 }
